fe36b1e3b211a6bd80614831d77a4db29557d199
Aswanth <aswanth2000hb@gmail.com>
2026-03-26 13:26:27 +1300

    Angular  v21 version bump

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module'; // Import your AppModule
 
-
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, {
+    applicationProviders: [provideZoneChangeDetection()]
+  })
   .catch(err => console.error(err));
